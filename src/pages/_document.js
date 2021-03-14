cd5ee5932080980502f60ee1d7ba3493dd5d4d90
Jared <jaredjgebel@gmail.com>
2021-03-14 17:13:32 -0400

    Add Open Graph meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,6 +15,18 @@ export default class Document extends NextDocument {
             name="description"
             content="Web developer portfolio for Jared Gebel. Includes examples of web applications built in React, Express, and Nextjs."
           />
+
+          {/* Open Graph meta tags for social media */}
+          <meta
+            property="og:title"
+            content="Jared Gebel's web developer portfolio"
+          />
+          <meta
+            property="og:description"
+            content="Web developer portfolio for Jared Gebel. Includes examples of web applications built in React, Express, and Nextjs."
+          />
+          <meta property="og:image" content="/portfolio-screenshot.jpg" />
+          <meta property="og:type" content="website" />
         </Head>
         <body>
           <Main />
